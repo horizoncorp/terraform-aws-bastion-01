5cdbf463a2c3ba1d9f0884d9c1c04bfc26dee777
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-05-30 17:17:14 +0530

    Add main tf

---
+++ ami.tf
@@ -0,0 +1,16 @@
+data "aws_ami" "bastion_ami" {
+  most_recent = "true"
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+}
