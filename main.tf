799a220aff38445c32f900877d7ed3ef2bd678d8
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-05-30 18:49:30 +0530

    Updating

--- main.tf
+++ main.tf
@@ -1,4 +1,28 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.bastion_ami.id
-  instance_type = var.instance_type
+resource "tls_private_key" "bastion_private_ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
+
+resource "aws_key_pair" "generated_key" {
+  key_name = "${var.environment}-bastion"
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.bastion_ami.id
+  instance_type               = var.instance_type
+  ebs_optimized               = true
+  monitoring                  = false
+  key_name                    = aws_key_pair.generated_key.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = ["value"]
+  associate_public_ip_address = false
+  source_dest_check           = true
+  user_data                   = templatefile("${path.module}/userdata.tpl")
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 128
+    delete_on_termination = true
+    encrypted             = true
+  }
+}
+
