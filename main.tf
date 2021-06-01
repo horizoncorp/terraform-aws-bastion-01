9b22cef535d40599f619bc0fe037422b49d01810
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 16:13:40 +0530

    Test

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ data "template_file" "init" {
   template = "${file("${path.module}/init.tpl")}"
 }
 
+module "sg" {
+  source = "github.com/horizoncorp/terraform-aws-securitygroup-01"
+  name   = "sg"
+  vpc_id = var.vpc_id
+}
+
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.bastion_ami.id
   instance_type               = var.instance_type
@@ -19,7 +25,7 @@ resource "aws_instance" "bastion" {
   monitoring                  = false
   key_name                    = aws_key_pair.generated_key.key_name
   subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = ["value"]
+  vpc_security_group_ids      = [module.sg.sg_id]
   associate_public_ip_address = false
   source_dest_check           = true
   user_data                   = join("\n", [data.template_file.init.rendered, var.user_data])
@@ -31,3 +37,9 @@ resource "aws_instance" "bastion" {
   }
 }
 
+module "sg-ssh" {
+  source = "github.com/horizoncorp/terraform-aws-securitygroup-01/modules/allow-ssh-22"
+  name   = "sg-ssh"
+  security_group_id = module.sg.sg_id
+  cidr_blocks = ["10.0.0.0/24"]
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "name" {}
 variable "region" {}
 variable "instance_type" {}
+variable "vpc_id" {}
 variable "subnet_id" {}
 variable "environment" {}
 variable "user_data" {}
