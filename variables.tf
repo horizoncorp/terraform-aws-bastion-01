5f47ba51b8498193fc4004ebe0e1c0e77bfd245f
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-02 12:55:46 +0530

    Update tags

--- variables.tf
+++ variables.tf
@@ -1,8 +1,6 @@
-variable "name" {}
 variable "region" {}
 variable "instance_type" {}
 variable "vpc_id" {}
 variable "subnet_id" {}
-variable "environment" {}
 variable "user_data" {}
 variable "tags" {}
